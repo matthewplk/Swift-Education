d3ea8be8b963b5c04670b3645c11a7059b55404f
matthewplk <matthewpolkofficial@gmail.com>
2025-02-11 13:59:27 -0600

    Initial Commit

---
+++ Swift-Setup/Swift_SetupApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_SetupApp.swift
+//  Swift-Setup
+//
+//  Created by Matthew Polk on 2/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Swift_SetupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Swift-SetupTests/Swift_SetupTests.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_SetupTests.swift
+//  Swift-SetupTests
+//
+//  Created by Matthew Polk on 2/11/25.
+//
+
+import Testing
+@testable import Swift_Setup
+
+struct Swift_SetupTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
